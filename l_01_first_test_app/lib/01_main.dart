cb14764a7d978675e0c104e18de49a92f2f53d83
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-02-06 15:21:50 +0400

    //Container Box Decoration, Border, Image ve Shadow

---
+++ l_01_first_test_app/lib/01_main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text(
+            "Header",
+          ),
+        ),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
+            child: Container(
+              width: 50,
+              height: 50,
+              margin: EdgeInsets.all(40),
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text("Gurban"),
+            ),
+            // padding: EdgeInsets.fromLTRB(30, 40, 30, 50),
+            // margin: EdgeInsets.fromLTRB(90, 40, 50, 80),
+            // constraints: BoxConstraints(maxHeight: 300, maxWidth: 300),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {debugPrint("clicked succesfull")},
+          child: Icon(
+            Icons.plus_one,
+            color: Colors.blue,
+          ),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
--- l_01_first_test_app/lib/main.dart
+++ l_01_first_test_app/lib/main.dart
@@ -1,3 +1,4 @@
+//Container Box Decoration, Border, Image ve Shadow
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,23 +18,27 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
             child: Container(
-              width: 50,
-              height: 50,
-              margin: EdgeInsets.all(40),
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: Text("Gurban"),
-            ),
-            // padding: EdgeInsets.fromLTRB(30, 40, 30, 50),
-            // margin: EdgeInsets.fromLTRB(90, 40, 50, 80),
-            // constraints: BoxConstraints(maxHeight: 300, maxWidth: 300),
+          padding: EdgeInsets.all(20),
+          child: FlutterLogo(
+            style: FlutterLogoStyle.stacked,
+            size: 128,
+            textColor: Colors.white,
           ),
-        ),
+          decoration: BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.rectangle,
+              border: Border.all(width: 10, color: Colors.white),
+              borderRadius: BorderRadius.circular(50),
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://media.evo.co.uk/image/private/s--M0x7cRgL--/v1608141046/evo/2020/12/BMW%20E39%20M5-3.jpg"),
+                  fit: BoxFit.cover),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.green, offset: Offset(0, 20), blurRadius: 10)
+              ]),
+        )),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {debugPrint("clicked succesfull")},
           child: Icon(
