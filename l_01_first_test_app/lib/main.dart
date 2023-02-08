092500417f8956a8e13b648d5c90f28bbf9461cf
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-02-08 18:29:46 +0400

    row column example

--- l_01_first_test_app/lib/main.dart
+++ l_01_first_test_app/lib/main.dart
@@ -1,4 +1,3 @@
-//Container Box Decoration, Border, Image ve Shadow
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,28 +16,37 @@ class MyApp extends StatelessWidget {
             "Header",
           ),
         ),
-        body: Center(
-            child: Container(
-          padding: EdgeInsets.all(20),
-          child: FlutterLogo(
-            style: FlutterLogoStyle.stacked,
-            size: 128,
-            textColor: Colors.white,
+        body: Container(
+          color: Colors.red,
+          height: 400,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.yellow,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.blue,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.teal,
+              )
+            ],
           ),
-          decoration: BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.rectangle,
-              border: Border.all(width: 10, color: Colors.white),
-              borderRadius: BorderRadius.circular(50),
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://media.evo.co.uk/image/private/s--M0x7cRgL--/v1608141046/evo/2020/12/BMW%20E39%20M5-3.jpg"),
-                  fit: BoxFit.cover),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.green, offset: Offset(0, 20), blurRadius: 10)
-              ]),
-        )),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {debugPrint("clicked succesfull")},
           child: Icon(
@@ -49,5 +57,31 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+//Container Box Decoration, Border, Image ve Shadow
+  Center containerExample() {
+    return Center(
+        child: Container(
+      padding: EdgeInsets.all(20),
+      child: FlutterLogo(
+        style: FlutterLogoStyle.stacked,
+        size: 128,
+        textColor: Colors.white,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.rectangle,
+          border: Border.all(width: 10, color: Colors.white),
+          borderRadius: BorderRadius.circular(50),
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://media.evo.co.uk/image/private/s--M0x7cRgL--/v1608141046/evo/2020/12/BMW%20E39%20M5-3.jpg"),
+              fit: BoxFit.cover),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.green, offset: Offset(0, 20), blurRadius: 10)
+          ]),
+    ));
   }
 }
